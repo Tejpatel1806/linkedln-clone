3086ce61bed3636e5f3a36a0f7b3f82a4e7e2ae1
Tejpatel1806 <tejpatel1806@gmail.com>
2022-12-10 16:08:45 -0800

    initial commit

---
+++ src/components/RightSide.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+const RightSide = (props) => {
+    return (
+        <Container>Right side</Container>
+        )
+};
+
+const Container=styled.div`
+   grid-area:rightside;
+`;
+
+export default RightSide;
